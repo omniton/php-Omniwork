23263c88f0de3243a1ed0b7d363ebfb1ebc02dd4
me <me@omniton-notebook.(none)>
2010-04-26 10:41:39 +0300

    functions cleaning

--- lib/functions.php
+++ lib/functions.php
@@ -5,11 +5,11 @@ function array_value(&$array, $value)
     return (isset($array[$value]) ? $array[$value] : null);
 }
 
-function unix_random_value()
+function unix_random_value($bytes = 16)
 {
-    $fp = @fopen('/dev/urandom','rb');
+    $fp = @fopen('/dev/urandom', 'rb');
     if ($fp !== FALSE) {
-        $random = @fread($fp,16);
+        $random = @fread($fp, $bytes);
         @fclose($fp);
     } else {
         $random = mt_rand(mt_getrandmax());
