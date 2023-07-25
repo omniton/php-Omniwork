50d400c74ce226429c7be7d2b5a78db91c4dad1e
omniton <omniton@users.noreply.github.com>
2023-07-25 11:04:03 +0300

    deleted folder

--- lib/Starburst/Debug/Console/Locale/en_US.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-    'TEXT_SOLAR_SQL'         => 'SQL Profiler',
-    'TEXT_SOLAR_LOG'         => 'Logs',
-    'TEXT_SOLAR_REQUEST'     => 'Environment',
-    'TEXT_SOLAR_DEBUG_TIMER' => 'Timer',
-);
